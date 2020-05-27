d22f28dbc6dc0c399d8235889a53bc3891edd364
Manan Desai <52067783+MananDesai54@users.noreply.github.com>
2020-05-28 01:25:59 +0530

    Add files via upload

---
+++ learning/osmodule/app.py
@@ -0,0 +1,13 @@
+import os;
+
+print(os.getcwd());
+# os.chdir('c://');
+print(os.listdir('c://'));
+# os.mkdir('this');
+# os.makedirs('that/these');
+# os.rename('osapp.py','app.py');
+# os.path.join
+# os.path.exists
+# os.environ.get
+# os.path.isdir
+# os.path.isfile
